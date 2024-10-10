47835227d606bdb4d65f128ce2507c49da55e9d0
Amon De Shir <amonsir29@gmail.com>
2024-10-10 04:55:32 +0200

    implement basic layout

--- src/main.rs
+++ src/main.rs
@@ -4,53 +4,14 @@
 use std::error::Error;
 use std::rc::Rc;
 use rand::prelude::SliceRandom;
-use slint::Model;
+use slint::{Model, PhysicalSize};
 
 slint::include_modules!();
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
-    let mut tiles = ui.get_memory_tiles().iter().collect::<Vec<TileData>>();
-    tiles.extend(tiles.clone());
-
-    let mut rng = rand::thread_rng();
-    tiles.shuffle(&mut rng);
-
-    let tiles_model = Rc::new(slint::VecModel::from(tiles));
-    ui.set_memory_tiles(tiles_model.clone().into());
-
-    let ui_weak = ui.as_weak();
-    ui.on_check_if_pair_solved(move || {
-        let mut flipped_tiles = tiles_model
-            .iter()
-            .enumerate()
-            .filter(|(_, tile)| tile.image_visible && !tile.solved);
-
-        if let (Some((t1_idx, mut t1)), Some((t2_idx, mut t2))) = (flipped_tiles.next(), flipped_tiles.next()) {
-            let is_pair_solved = t1 == t2;
-
-            if is_pair_solved {
-                t1.solved = true;
-                t2.solved = true;
-                tiles_model.set_row_data(t1_idx, t1);
-                tiles_model.set_row_data(t2_idx, t2);
-            }
-            else {
-                let ui = ui_weak.unwrap();
-
-                ui.set_disable_tiles(true);
-                let tiles_model = tiles_model.clone();
-                slint::Timer::single_shot(std::time::Duration::from_millis(300), move || {
-                    ui.set_disable_tiles(false);
-                    t1.image_visible = false;
-                    t2.image_visible = false;
-                    tiles_model.set_row_data(t1_idx, t1);
-                    tiles_model.set_row_data(t2_idx, t2);
-                });
-            }
-        }
-    });
+    ui.window().set_size(PhysicalSize::new(400, 660));
 
     ui.run()?;
     Ok(())
